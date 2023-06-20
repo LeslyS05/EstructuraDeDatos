bb2e17d93103981e9f5303f5f19d563c0f2450be
Lesly Tineo <94697931+LeslyS05@users.noreply.github.com>
2023-06-20 14:06:39 -0400

    Update codigo.cpp

--- SerieFibonacci /codigo.cpp
+++ SerieFibonacci /codigo.cpp
@@ -1,1 +1,21 @@
-.
+#include <iostream>
+#include <iostream>
+
+int main() {
+    int n;
+    std::cout << "Ingrese el número de términos de la serie Fibonacci que desea generar: ";
+    std::cin >> n;
+
+    int t1 = 0, t2 = 1;
+    std::cout << "Serie Fibonacci de " << n << " términos: ";
+
+    for (int i = 1; i <= n; ++i) {
+        std::cout << t1 << " ";
+
+        int sum = t1 + t2;
+        t1 = t2;
+        t2 = sum;
+    }
+
+    return 0;
+}
