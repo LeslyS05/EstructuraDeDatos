2256f81a00aed2e16f9c90e80f29751d4bad3901
Lesly Tineo <94697931+LeslyS05@users.noreply.github.com>
2023-10-02 18:28:09 -0400

    Create La-flota-de-camiones.cpp

---
+++ Serie-10maSemana/La-flota-de-camiones.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool cumpleRequisito(int num) {
+    int cuadrado = num * num;
+    int divisor = 10;
+    int numTemp = num;
+
+    while (numTemp > 0) {
+        if (numTemp % divisor != cuadrado % divisor)
+            return false;
+        
+        numTemp /= 10;
+        cuadrado /= 10;
+    }
+
+    return true;
+}
+
+int main() {
+    vector<int> camiones;
+
+    for (int i = 1; i < 500; i++) {
+        if (cumpleRequisito(i)) {
+            camiones.push_back(i);
+        }
+    }
+
+    cout << "Son " << camiones.size() << " camiones que cumplen el requisito:" << endl;
+    for (int i = 0; i < camiones.size(); i++) {
+        int cuadrado = camiones[i] * camiones[i];
+        cout << "(" << camiones[i] << " ^2 = " << cuadrado << ")" << endl;
+    }
+
+    return 0;
+}
